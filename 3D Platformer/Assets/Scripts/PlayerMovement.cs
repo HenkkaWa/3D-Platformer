e2faff5f3be21fb6088b8a7b7e3e3702946fe899
Henkka <henri.anttonen04@hotmail.com>
2023-08-31 20:40:20 +0300

    Better Movement

--- 3D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 3D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,30 +17,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown("space"))
-      {
-        rb.velocity = new Vector3(0, 5, 0);
-      }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-      if (Input.GetKey("up"))
-      {
-        rb.velocity = new Vector3(0, 0, 5);
-      }
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
-      if (Input.GetKey("down"))
-      {
-        rb.velocity = new Vector3(0, 0, -5);
-      }  
 
-      if (Input.GetKey("right"))
-      {
-        rb.velocity = new Vector3(5, 0, 0);
-      }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
 
-      if (Input.GetKey("left"))
-      {
-        rb.velocity = new Vector3(-5, 0, 0);
-      }
 
     }
 }
