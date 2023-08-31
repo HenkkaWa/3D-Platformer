6944f7b398cc075dd960e1b0a8a1ab3bbe96534d
Henkka <henri.anttonen04@hotmail.com>
2023-08-31 20:16:07 +0300

    Added Better Movement

--- 3D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 3D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,27 +16,27 @@ public class PlayerMovement : MonoBehaviour
     {
       if (Input.GetKeyDown("space"))
       {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+        rb.velocity = new Vector3(0, 5, 0);
       }
 
       if (Input.GetKey("up"))
       {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        rb.velocity = new Vector3(0, 0, 5);
       }
 
       if (Input.GetKey("down"))
       {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
+        rb.velocity = new Vector3(0, 0, -5);
       }  
 
       if (Input.GetKey("right"))
       {
-        GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+        rb.velocity = new Vector3(5, 0, 0);
       }
 
       if (Input.GetKey("left"))
       {
-        GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+        rb.velocity = new Vector3(-5, 0, 0);
       }
 
     }
